7988756133ef2728cddcf3ad96e31ba42fd24afc
EduardoMilione2005 <145071121+EduardoMilione2005@users.noreply.github.com>
2026-03-31 15:42:44 -0300

    Tirando mais comentários
    Removed comment about the Bridge pattern from ControleRemoto class.

--- controle-remoto/src/main/java/com/controle/controle/ControleRemoto.java
+++ controle-remoto/src/main/java/com/controle/controle/ControleRemoto.java
@@ -4,7 +4,6 @@ import com.controle.dispositivo.Dispositivo;
 
 public abstract class ControleRemoto {
 
-    // Referência para o Implementor - núcleo do padrão Bridge
     protected Dispositivo dispositivo;
 
     public ControleRemoto(Dispositivo dispositivo) {
